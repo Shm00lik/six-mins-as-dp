c5764fdd491cd37eceeb5ff5f218463843992d99
Shm00lik <70761433+Shm00lik@users.noreply.github.com>
2024-10-24 00:23:22 +0300

    initialized firebase and started working on the timer

--- src/game/definitions.ts
+++ src/game/definitions.ts
@@ -1,70 +1,77 @@
 export const Definitions = {
-    grid: {
-        rows: 15,
-        cols: 10,
-        gridHeight: 0.7,
+  grid: {
+    rows: 15,
+    cols: 10,
+    gridHeight: 0.7,
 
-        rolesList: [
-            "מעבד נתונים",
-            "אחמש",
-            "סקאוטר",
-            "סקאוטר BAS",
-            "אחראי BAS",
-            "איכותני",
-            "צוות אדום",
-            "פיט מכניקה",
-            "פיט חומרה",
-            "פיט תוכנה",
-            "פיט שרטוט",
-            "אחראי פיט",
-            "דוברות פיט",
-            "דוברות קהילה",
-            "נהג א'",
-            "נהג ב'",
-            "מפעיל א'",
-            "מפעיל ב'",
-            "קואוץ' א'",
-            "קואוץ' ב'",
-            "שחקנש",
-            "צלם מגרש",
-            "טכנישן",
-            "צלם מדיה",
-            "אחראי עידוד",
-            "קמע",
-            "לוגיסטיקה",
-        ],
+    rolesList: [
+      "מעבד נתונים",
+      "אחמש",
+      "סקאוטר",
+      "סקאוטר BAS",
+      "אחראי BAS",
+      "איכותני",
+      "צוות אדום",
+      "פיט מכניקה",
+      "פיט חומרה",
+      "פיט תוכנה",
+      "פיט שרטוט",
+      "אחראי פיט",
+      "דוברות פיט",
+      "דוברות קהילה",
+      "נהג א'",
+      "נהג ב'",
+      "מפעיל א'",
+      "מפעיל ב'",
+      "קואוץ' א'",
+      "קואוץ' ב'",
+      "שחקנש",
+      "צלם מגרש",
+      "טכנישן",
+      "צלם מדיה",
+      "אחראי עידוד",
+      "קמע",
+      "לוגיסטיקה",
+    ],
 
-        rolesTextStyle: {
-            font: "20px Assistant",
-            fontStyle: "bold",
-            color: "#000000",
-            rtl: true,
-        },
-
-        cell: {
-            fillColor: 0xffffff,
-            strokeColor: 0xe1e1e1,
-            lineWidth: 4,
-        },
+    rolesTextStyle: {
+      font: "20px Assistant",
+      fontStyle: "bold",
+      color: "#000000",
+      rtl: true,
     },
 
-    background: {
-        path: "./src/assets/background.png",
+    cell: {
+      fillColor: 0xffffff,
+      strokeColor: 0xe1e1e1,
+      lineWidth: 4,
     },
+  },
 
-    ball: {
-        path: "./src/assets/ball.png",
-        size: 100,
-    },
+  background: {
+    path: "./src/assets/background.png",
+  },
 
-    shooter: {
-        body: {
-            path: "./src/assets/shooterBody.png",
-            scale: 0.3,
-        },
-        leg: {
-            path: "./src/assets/shooterLeg.png",
-            scale: 0.3,
-        },
+  ball: {
+    path: "./src/assets/ball.png",
+    size: 100,
+  },
+
+  shooter: {
+    body: {
+      path: "./src/assets/shooterBody.png",
+      scale: 0.3,
     },
+    leg: {
+      path: "./src/assets/shooterLeg.png",
+      scale: 0.3,
+    },
+  },
+
+  timer: {
+    textStle: {
+      font: "100px Assistant",
+      color: "#FFFFFF",
+    },
+  },
 };
--- src/game/scenes/MainScene.ts
+++ src/game/scenes/MainScene.ts
@@ -1,242 +1,248 @@
+import { Display } from "phaser";
 import { Definitions } from "../definitions";
 
 enum LookingDirection {
-    Left = 0,
-    Right = 1,
+  Left = 0,
+  Right = 1,
 }
 class MainScene extends Phaser.Scene {
-    ball?: Phaser.Types.Physics.Arcade.ImageWithDynamicBody;
-    grid?: Phaser.Physics.Arcade.StaticGroup;
-    shooterContainer?: Phaser.GameObjects.Container;
-    shooterLeg?: Phaser.GameObjects.Sprite;
-    isShooting: boolean = false;
-    isBallInAir: boolean = false;
-    lookingDirection: LookingDirection = LookingDirection.Right;
+  ball?: Phaser.Types.Physics.Arcade.ImageWithDynamicBody;
+  grid?: Phaser.Physics.Arcade.StaticGroup;
+  shooterContainer?: Phaser.GameObjects.Container;
+  shooterLeg?: Phaser.GameObjects.Sprite;
+  timer?: Phaser.GameObjects.Text;
+  isShooting: boolean = false;
+  isBallInAir: boolean = false;
+  lookingDirection: LookingDirection = LookingDirection.Right;
 
-    constructor() {
-        super({ key: "MainScene" });
-    }
+  constructor() {
+    super({ key: "MainScene" });
+  }
 
-    preload() {
-        this.load.image("background", Definitions.background.path);
-        this.load.image("ball", Definitions.ball.path);
-        this.load.image("shooter", Definitions.shooter.body.path);
-        this.load.image("shooterLeg", Definitions.shooter.leg.path);
-    }
+  preload() {
+    this.load.image("background", Definitions.background.path);
+    this.load.image("ball", Definitions.ball.path);
+    this.load.image("shooter", Definitions.shooter.body.path);
+    this.load.image("shooterLeg", Definitions.shooter.leg.path);
+  }
 
-    create() {
-        this.loadBackground();
-        this.createGrid();
-        this.createBall();
-        this.createShooter();
+  create() {
+    this.loadBackground();
+    this.createGrid();
+    this.createTimer();
+    this.createShooter();
+    this.createBall();
 
-        this.input.on("pointerdown", (point: any) => {
-            this.onPointerDown(point);
-        });
+    this.input.on("pointerdown", (point: any) => {
+      this.onPointerDown(point);
+    });
 
-        this.input.on("pointermove", (pointer: Phaser.Input.Pointer) => {
-            this.shooterContainer?.setPosition(
-                pointer.x,
-                this.shooterContainer.y
-            );
+    this.input.on("pointermove", (pointer: Phaser.Input.Pointer) => {
+      this.shooterContainer?.setPosition(pointer.x, this.shooterContainer.y);
 
-            if (!this.isBallInAir)
-                this.ball?.setPosition(
-                    pointer.x + (this.lookingDirection ? 170 : -170),
-                    this.ball.y
-                );
-        });
-    }
-
-    update() {
-        if (this.ball?.body.blocked.down) {
-            const randSide = Math.round(Math.random());
-
-            if (randSide == LookingDirection.Left) {
-                this.lookingDirection = LookingDirection.Left;
-                this.shooterContainer?.setScale(-1, 1);
-            } else {
-                this.lookingDirection = LookingDirection.Right;
-                this.shooterContainer?.setScale(1, 1);
-            }
-
-            this.resetBall();
-        }
-
-        // if (this.shooterContainer && ) {
-        //     if (
-        //         this.shooterContainer.x >= this.game.canvas.width / 2 + 300 &&
-        //         this.lookingDirection === LookingDirection.Left
-        //     ) {
-        //         this.shooterContainer?.setScale(1, 1);
-        //         this.lookingDirection = LookingDirection.Right;
-        //     } else if (
-        //         this.shooterContainer.x <= this.game.canvas.width / 2 - 300 &&
-        //         this.lookingDirection === LookingDirection.Right
-        //     ) {
-        //         this.shooterContainer?.setScale(-1, 1);
-        //         this.lookingDirection = LookingDirection.Left;
-        //     }
-        // }
-    }
-
-    private loadBackground() {
-        this.add.image(
-            this.game.canvas.width / 2,
-            this.game.canvas.height / 2,
-            "background"
+      if (!this.isBallInAir)
+        this.ball?.setPosition(
+          pointer.x + (this.lookingDirection ? 170 : -170),
+          this.ball.y
         );
-    }
-    private createGrid() {
-        this.grid = this.physics.add.staticGroup();
+    });
+  }
 
-        const cellWidth = this.game.canvas.width / Definitions.grid.cols;
-        const cellHeight =
-            (this.game.canvas.height / Definitions.grid.rows) *
-            Definitions.grid.gridHeight;
+  update() {
+    if (this.ball?.body.blocked.down && this.isBallInAir) {
+      const randSide = Math.round(Math.random());
 
-        for (let i = 0; i < Definitions.grid.cols; i++) {
-            for (let j = 0; j < Definitions.grid.rows; j++) {
-                const x = i * cellWidth;
-                const y = j * cellHeight;
+      if (randSide == LookingDirection.Left) {
+        this.lookingDirection = LookingDirection.Left;
+        this.shooterContainer?.setScale(-1, 1);
+      } else {
+        this.lookingDirection = LookingDirection.Right;
+        this.shooterContainer?.setScale(1, 1);
+      }
 
-                // Rectangle
-                const rect = this.add
-                    .rectangle(
-                        x + cellWidth / 2,
-                        y + cellHeight / 2,
-                        cellWidth,
-                        cellHeight,
-                        Definitions.grid.cell.fillColor,
-                        1
-                    )
-                    .setStrokeStyle(
-                        Definitions.grid.cell.lineWidth,
-                        Definitions.grid.cell.strokeColor
-                    );
-
-                // Text
-                const text = this.add
-                    .text(
-                        x + cellWidth / 2,
-                        y + cellHeight / 2,
-                        Definitions.grid.rolesList[
-                            Math.floor(
-                                Math.random() *
-                                    Definitions.grid.rolesList.length
-                            )
-                        ],
-                        Definitions.grid.rolesTextStyle
-                    )
-                    .setOrigin(0.5, 0.5);
-
-                this.grid.add(rect.setData("text", text));
-            }
-        }
+      this.resetBall();
     }
 
-    private createBall() {
-        this.ball = this.physics.add.image(1130, 920, "ball");
-        this.setBallGlow(0xffdd1f);
-        this.ball.setDisplaySize(Definitions.ball.size, Definitions.ball.size);
-        this.ball.setCollideWorldBounds(true);
-        this.ball.setBounce(1);
+    this.updateTimer();
+  }
 
-        this.physics.add.collider(this.ball, this.grid!, this.onCollide);
+  private loadBackground() {
+    this.add.image(
+      this.game.canvas.width / 2,
+      this.game.canvas.height / 2,
+      "background"
+    );
+  }
+
+  private createGrid() {
+    this.grid = this.physics.add.staticGroup();
+
+    const cellWidth = this.game.canvas.width / Definitions.grid.cols;
+    const cellHeight =
+      (this.game.canvas.height / Definitions.grid.rows) *
+      Definitions.grid.gridHeight;
+
+    for (let i = 0; i < Definitions.grid.cols; i++) {
+      for (let j = 0; j < Definitions.grid.rows; j++) {
+        const x = i * cellWidth;
+        const y = j * cellHeight;
+
+        // Rectangle
+        const rect = this.add
+          .rectangle(
+            x + cellWidth / 2,
+            y + cellHeight / 2,
+            cellWidth,
+            cellHeight,
+            Definitions.grid.cell.fillColor,
+            1
+          )
+          .setStrokeStyle(
+            Definitions.grid.cell.lineWidth,
+            Definitions.grid.cell.strokeColor
+          );
+
+        // Text
+        const text = this.add
+          .text(
+            x + cellWidth / 2,
+            y + cellHeight / 2,
+            Definitions.grid.rolesList[
+              Math.floor(Math.random() * Definitions.grid.rolesList.length)
+            ],
+            Definitions.grid.rolesTextStyle
+          )
+          .setOrigin(0.5, 0.5);
+
+        this.grid.add(rect.setData("text", text));
+      }
     }
+  }
 
-    private setBallGlow(color: number) {
-        this.ball?.clearFX();
-        this.ball?.preFX?.addGlow(color);
-    }
+  private createBall() {
+    this.ball = this.physics.add.image(1130, 920, "ball");
+    this.setBallGlow(0xffdd1f);
+    this.ball.setToTop();
+    this.ball.setDisplaySize(Definitions.ball.size, Definitions.ball.size);
+    this.ball.setCollideWorldBounds(true);
+    this.ball.setBounce(1);
 
-    private resetBall() {
-        this.setBallGlow(0xffdd1f);
-        this.ball?.setVelocity(0, 0);
+    this.physics.add.collider(this.ball, this.grid!, this.onCollide);
+  }
 
-        this.tweens.add({
-            targets: this.ball,
-            x: this.shooterContainer!.x + (this.lookingDirection ? 170 : -170),
-            y: 920,
-            duration: 100,
-            onComplete: () => {
-                this.isBallInAir = false;
-            },
-        });
-    }
+  private setBallGlow(color: number) {
+    this.ball?.clearFX();
+    this.ball?.preFX?.addGlow(color);
+  }
 
-    private createShooter() {
-        const shooterContainer = this.add.container(
-            this.game.canvas.width / 2,
-            this.game.canvas.height
-        );
+  private resetBall() {
+    this.setBallGlow(0xffdd1f);
+    this.ball?.setVelocity(0, 0);
 
-        const shooter = this.add.sprite(0, 0, "shooter");
-        shooter.postFX?.addGlow(0xf5d409);
-        shooter.setOrigin(0.5, 1);
-        shooter.setScale(Definitions.shooter.body.scale);
-        shooterContainer.add(shooter);
+    this.tweens.add({
+      targets: this.ball,
+      x: this.shooterContainer!.x + (this.lookingDirection ? 170 : -170),
+      y: 920,
+      duration: 100,
+      onComplete: () => {
+        this.isBallInAir = false;
+      },
+    });
+  }
 
-        const shooterLeg = this.add.sprite(-7, -170, "shooterLeg");
-        shooterLeg.postFX?.addGlow(0xf5d409);
-        shooterLeg.setOrigin(0, 0);
-        shooterLeg.setScale(Definitions.shooter.leg.scale);
-        shooterContainer.add(shooterLeg);
+  private createShooter() {
+    const shooterContainer = this.add.container(
+      this.game.canvas.width / 2,
+      this.game.canvas.height
+    );
 
-        this.shooterLeg = shooterLeg;
-        this.shooterContainer = shooterContainer;
-    }
+    const shooter = this.add.sprite(0, 0, "shooter");
+    shooter.postFX?.addGlow(0xf5d409);
+    shooter.setOrigin(0.5, 1);
+    shooter.setScale(Definitions.shooter.body.scale);
+    shooterContainer.add(shooter);
 
-    private kickAnimation() {
-        this.tweens.add({
-            targets: this.shooterLeg,
-            angle: -60,
-            duration: 100,
-            yoyo: true,
-            onStart: () => {
-                this.isShooting = true;
-            },
-            onComplete: () => {
-                this.isShooting = false;
-            },
+    const shooterLeg = this.add.sprite(-7, -170, "shooterLeg");
+    shooterLeg.postFX?.addGlow(0xf5d409);
+    shooterLeg.setOrigin(0, 0);
+    shooterLeg.setScale(Definitions.shooter.leg.scale);
+    shooterContainer.add(shooterLeg);
 
-            onYoyo: () => {
-                this.shootBall();
-            },
-        });
-    }
+    this.shooterLeg = shooterLeg;
+    this.shooterContainer = shooterContainer;
+  }
 
-    private onCollide = (
-        ball:
-            | Phaser.Types.Physics.Arcade.GameObjectWithBody
-            | Phaser.Physics.Arcade.Body
-            | Phaser.Tilemaps.Tile,
-        cell:
-            | Phaser.Types.Physics.Arcade.GameObjectWithBody
-            | Phaser.Physics.Arcade.Body
-            | Phaser.Tilemaps.Tile
-    ) => {
-        const rect = cell as Phaser.GameObjects.Rectangle;
-        const cellRect = rect.getData("text") as Phaser.GameObjects.Text;
+  private kickAnimation() {
+    this.tweens.add({
+      targets: this.shooterLeg,
+      angle: -60,
+      duration: 100,
+      yoyo: true,
+      onStart: () => {
+        this.isShooting = true;
+      },
+      onComplete: () => {
+        this.isShooting = false;
+      },
 
-        rect.destroy();
-        cellRect.destroy();
-    };
+      onYoyo: () => {
+        this.shootBall();
+      },
+    });
+  }
 
-    private shootBall() {
-        this.physics.velocityFromAngle(
-            this.lookingDirection ? -60 : -120,
-            1000,
-            this.ball?.body.velocity
-        );
-        this.setBallGlow(0x00ff00);
+  private onCollide = (
+    ball:
+      | Phaser.Types.Physics.Arcade.GameObjectWithBody
+      | Phaser.Physics.Arcade.Body
+      | Phaser.Tilemaps.Tile,
+    cell:
+      | Phaser.Types.Physics.Arcade.GameObjectWithBody
+      | Phaser.Physics.Arcade.Body
+      | Phaser.Tilemaps.Tile
+  ) => {
+    const rect = cell as Phaser.GameObjects.Rectangle;
+    const cellRect = rect.getData("text") as Phaser.GameObjects.Text;
 
-        this.isBallInAir = true;
-    }
+    rect.destroy();
+    cellRect.destroy();
+  };
 
-    private onPointerDown = (pointer: Phaser.Input.Pointer) => {
-        if (!this.isShooting && !this.isBallInAir) this.kickAnimation();
-    };
+  private shootBall() {
+    this.physics.velocityFromAngle(
+      this.lookingDirection ? -60 : -120,
+      1000,
+      this.ball?.body.velocity
+    );
+    this.setBallGlow(0x00ff00);
+
+    this.isBallInAir = true;
+  }
+
+  private onPointerDown = (pointer: Phaser.Input.Pointer) => {
+    if (!this.isShooting && !this.isBallInAir) this.kickAnimation();
+  };
+
+  private createTimer() {
+    this.timer = this.add.text(0, 950, "00:00", Definitions.timer.textStle);
+
+    this.timer.setOrigin(0, 1);
+    this.timer.setPosition(20, this.game.canvas.height);
+  }
+
+  private updateTimer = () => {
+    const time = this.time.now;
+    const minutes = Math.floor(time / 60000);
+    const seconds = Math.floor((time % 60000) / 1000);
+
+    this.timer?.setText(
+      `${minutes < 10 ? "0" + minutes : minutes}:${
+        seconds < 10 ? "0" + seconds : seconds
+      }`
+    );
+
+    
+  };
 }
 
 export default MainScene;
